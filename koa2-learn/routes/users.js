d240dac9a150ffc5f1cc9e17d0ff580505217be3
wenyajun <153165057@qq.com>
2019-07-31 18:13:04 +0800

    修改users.js

--- koa2-learn/routes/users.js
+++ koa2-learn/routes/users.js
@@ -1,4 +1,6 @@
 const router = require('koa-router')()
+const Person = require('../dbs/models/person')
+
 
 router.prefix('/users')
 
@@ -8,6 +10,56 @@ router.get('/', function (ctx, next) {
 
 router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
+})
+
+router.post('/addPerson',async function(ctx){
+  const person = new Person({
+    name:ctx.request.body.name,
+    adge:ctx.request.body.age
+  })
+  let code 
+  try {
+    await person.save();
+    code = 0;
+  } catch (error) {
+    code = -1;
+  }
+  
+  ctx.body = {
+    code:code
+  }
+})
+
+router.post('/getPerson',async function(ctx){w3
+  const result = await Person.findOne({name:ctx.request.body.name})
+  const results  = await Person.find({name:ctx.request.body.name})
+  ctx.body = {
+    code:0,
+    result,
+    results
+  }
+})
+
+router.post('/updatePerson',async function(ctx){
+  const result = await Person.where({
+    name:ctx.request.body.name
+  }).update({
+    age:ctx.request.body.age
+  })
+  ctx.body = {
+    code:0
+  }
+
+})
+
+router.post('/removePerson',async function(ctx){
+  const result = await Person.where({
+    name:ctx.request.body.name
+  }).remove()
+  ctx.body = {
+    code:099999
+  }
+
 })
 
 module.exports = router
